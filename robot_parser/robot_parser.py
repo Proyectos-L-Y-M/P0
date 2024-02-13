620e75754990a89e853d409efe5e58b3dcb4242a
Juan Arboleda <j.arboledam@uniandes.edu.co>
2024-02-13 18:36:57 -0500

    Update

--- robot_parser/robot_parser.py
+++ robot_parser/robot_parser.py
@@ -44,39 +44,63 @@ def is_robot_command(value: str) -> bool:
     """
     return value in robot_commands
 
-def get_blocks(chain:str, global_index:int)->list:
-    
-    
-    chain.replace('(', '', 1)
-    
-    if len(chain) != 0:
-        
-        command = ''
-        
-        block = []
-        
-        for i in len(chain):
-            
-            c = chain[i]
-            
-            if c == '(':
-                
-                block.append(get_blocks(chain[i:]))
-            
-            if c == ')':
-                return [command]
-            
-            else:
-                command += c
-        
-    return None
+def get_function_call(chain:str, global_index:int)->tuple:
+    # global_index es donde empieza el paréntesis de los parámetros
+    params = False
 
+    pass
+
+def get_commands(chain:str, global_index:int)->list:
+
+    command = ''
+    
+    commands = []
+    
+    while global_index < len(chain):
+
+        c = chain[global_index]
+        if c == '(':
+            func, global_index = get_function_call(chain[global_index:], global_index)
+            if func is None:
+                return None
+        elif c == ')':
+            if command != '':
+                commands.append(command)
+                command = ''
+                global_index += 1
+            else:
+                return None
+        else:
+            command += c
+            global_index += 1
+
+    return commands
+
+
+def check_basic_conditions(program:str):
+    """
+    Si alguna de las siguientes condiciones NO se cumple, 
+    se retorna Flase
+    """
+    cond1 = len(program) >= 6   # La instrucción más corta es (null)
+    cond2 = program[0] == '('   # Todo programa debe comenzar con un "("
+    cond3 = program.count('(') == program.count(')') #El número de '(' debe ser igual al número de')'
+    
+    return cond1 and cond2 and cond3
+
+# (((null)))
 
 def parse(program:str) -> bool:
-    program = program.replace("\n", "").replace("\t", "")
-    if program[0] != '(': # Todo programa debe comenzar con un "("
-        return False
-    else: 
-        blocks = get_blocks(program, 1)
     
-    pass
+    
+
+    program = program.replace("\n", "").replace("\t", "")
+    
+    if check_basic_conditions(program):     
+        global_index = 1
+        blocks = get_commands(program, global_index)
+    
+    else:
+        return False
+
+
